9c80f14d686f5e284189b2dea864009b0a5130e8
Ratih Paramita <significantratih@gmail.com>
2024-09-29 03:37:01 +0700

    Pertemuan 5-Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\LevelController;
+/*use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LevelController;*/
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
 
@@ -28,5 +28,16 @@ Route::get('/user/tambah', [UserController::class, 'tambah']);
 Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);*/
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function(){
+    Route::get('/', [UserController::class, 'index']);              //menampilkan laman awal user
+    Route::post('/list', [UserController::class, 'list']);          //menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);       //menampilkan laman form tambah user
+    Route::post('/', [UserController::class, 'store']);             //menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);           //menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);      //menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);         //menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']);     //menghapus data user
+});
