871979187ef71fb0e0e78db219b7426b3f903140
Ratih Paramita <significantratih@gmail.com>
2024-09-30 11:05:59 +0700

    Pertemuan 6-Praktikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,8 @@ use App\Models\LevelModel;   //mengimpor model LevelModel
 use App\Models\UserModel;   //mengimpor model UserModel
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;   //mengimpor validator
+
 //use Illuminate\Support\Facades\Hash;    //mengimpor kelas Hash
 
 class UserController extends Controller
@@ -225,6 +227,45 @@ class UserController extends Controller
         ]);
 
         return redirect('/user')->with('success', 'Data user berhasil disimpan');
+    }
+
+    //Menampilkan laman form tambah user baru AJAX
+    public function create_ajax()
+    {
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('user.create_ajax')
+                    ->with('level', $level);
+    }
+
+    //Menyimpan data user baru AJAX
+    public function store_ajax(Request $request){
+        //periksa bila request berupa AJAX atau tidak
+        if($request->ajax() || $request->wantsJson()){
+            $rules = [
+                'level_id'  => 'required|integer',
+                'username'  => 'required|string|min:3|unique:m_user,username',
+                'nama'      => 'required|string|max:100',
+                'password'  => 'required|min:6'
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if($validator->fails()){
+                return response()->json([
+                    'status'    =>false,    //response status, false: eror/gagal, true:berhasil
+                    'message'   => 'Validasi Gagal',
+                    'msgField'  => $validator->errors(),    //pesan eror validasi
+                ]);
+            }
+
+            UserModel::create($request->all());
+            return response()->json([
+                'status'    => true,
+                'message'   => 'Data user berhasil disimpan'
+            ]);
+        }
+        redirect('/');
     }
 
     //Menampilkan detil data user
--- routes/web.php
+++ routes/web.php
@@ -38,14 +38,16 @@ Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);*/
 Route::get('/', [WelcomeController::class, 'index']);
 
 Route::group(['prefix' => 'user'], function(){
-    Route::get('/', [UserController::class, 'index']);              //menampilkan laman awal user
-    Route::post('/list', [UserController::class, 'list']);          //menampilkan data user dalam bentuk json untuk datatables
-    Route::get('/create', [UserController::class, 'create']);       //menampilkan laman form tambah user
-    Route::post('/', [UserController::class, 'store']);             //menyimpan data user baru
-    Route::get('/{id}', [UserController::class, 'show']);           //menampilkan detail user
-    Route::get('/{id}/edit', [UserController::class, 'edit']);      //menampilkan laman form edit user
-    Route::put('/{id}', [UserController::class, 'update']);         //menyimpan perubahan data user
-    Route::delete('/{id}', [UserController::class, 'destroy']);     //menghapus data user
+    Route::get('/', [UserController::class, 'index']);                  //menampilkan laman awal user
+    Route::post('/list', [UserController::class, 'list']);              //menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);           //menampilkan laman form tambah user
+    Route::post('/', [UserController::class, 'store']);                 //menyimpan data user baru
+    Route::get('/create_ajax', [UserController::class, 'create_ajax']); //menampilkan laman form tambah user AJAX
+    Route::post('/ajax', [UserController::class, 'store_ajax']);        //menyimpan data user baru AJAX
+    Route::get('/{id}', [UserController::class, 'show']);               //menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);          //menampilkan laman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);             //menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']);         //menghapus data user
 });
 
 Route::group(['prefix' => 'level'], function(){
