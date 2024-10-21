32ed53cf87074f29853e3ef2ba1d8fe8b6844d47
Ratih Paramita <significantratih@gmail.com>
2024-10-21 12:05:27 +0700

    UTS-Praktikum 1.5

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -349,6 +349,7 @@ class BarangController extends Controller
                     ->orderBy('kategori_id')
                     ->with('kategori')
                     ->get();
+                    //dd($barang);
         //load library excel
         $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet(); //ambil sheet yang aktif
@@ -380,14 +381,15 @@ class BarangController extends Controller
         $sheet->setTitle('Data Barang'); //set title sheet
 
         $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
-        $filename = 'Data Barang' . date('Y-m-d H:i:s') . '.xlsx';
+        $filename = 'Data Barang ' . date('Y-m-d H:i:s') . '.xlsx';
+        //dd($filename);
 
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="'.$filename.'"');
+        header('Content-Disposition: attachment; filename="' . $filename . '"');
         header('Cache-Control: max-age=0');
         header('Cache-Control: max-age=1');
         header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-        header('Last Modified: '. gmdate('D, d M Y H:i:s') . ' GMT');
+        header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
         header('Cache-Control: cache, must-revalidate');
         header('Pragma: public');
 
